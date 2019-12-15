2ab75e14e9ffb4469da904de18536143f09485c8
junfrenkixp <penin13579@gmail.com>
2019-12-15 04:14:02 +0300

    add 3-module

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,14 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  const reg = /(-?\d+.?\d+)/g;
+  let arr = str.match(reg);
+  arr.forEach((el, i) => {
+    arr[i] = parseFloat(el);
+  });
+  const obj = {
+    min: Math.min.apply(null, arr),
+    max: Math.max.apply(null, arr),
+  };
+  return obj;
 }
