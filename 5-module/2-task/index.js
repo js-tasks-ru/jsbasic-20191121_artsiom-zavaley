a5dcf66424f9e921997425086e745486df815a6a
junfrenkixp <penin13579@gmail.com>
2019-12-22 04:42:16 +0300

    5-module 2-task add style for table

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -18,6 +18,7 @@ function SortableTable(items) {
    * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
   this.el = document.createElement('table');
+  this.el.classList.add('table');
 
   /**
    * Метод выполняет сортировку таблицы
@@ -29,23 +30,27 @@ function SortableTable(items) {
 
   let thead = document.createElement('thead');
   let trhead = document.createElement('tr');
+  trhead.classList.add('table__row_head');
   let headInfo = ['Name', 'Age', 'Salary', 'City']; 
 
   headInfo.forEach(item => {
     let tdhead = document.createElement('td');
+    tdhead.classList.add('table__item');
     tdhead.innerHTML = item;
     trhead.appendChild(tdhead);
   });
   thead.appendChild(trhead);
   this.el.appendChild(thead);
 
-  
+
   let tbody = document.createElement('tbody');
 
   items.forEach(item => {
     let trbody = document.createElement('tr');
+    trbody.classList.add('table__row');
     for (let key in item) {
       let tdbody = document.createElement('td');
+      tdbody.classList.add('table__item');
       tdbody.innerHTML = item[key];
       trbody.appendChild(tdbody);
       tbody.appendChild(trbody);
@@ -54,5 +59,27 @@ function SortableTable(items) {
   this.el.appendChild(tbody);
 
 
-  this.sort = (column, desc = false) => {};
+  this.sort = (column, desc = false) => {
+    let trs = Array.from(tbody.rows);
+
+    if (column === 0) {
+      trs.sort((rowA, rowB) => {
+        return rowA.cells[column].innerHTML > rowB.cells[column].innerHTML ? 1 : -1;
+      });
+      if (desc === true) {
+        trs.reverse();
+      }
+      tbody.append(...trs);
+    }
+
+    if (column === 2) {      
+      trs.sort((rowA, rowB) => {
+        return rowA.cells[column].innerHTML - rowB.cells[column].innerHTML;
+      });
+      if (desc === true) {
+        trs.reverse();
+      }
+      tbody.append(...trs);
+    }
+  };
 }
