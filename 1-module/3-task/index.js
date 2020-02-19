afa72f33386591a40144ee6dbfb4a46381c2fd3c
junfrenkixp <penin13579@gmail.com>
2020-02-20 01:29:06 +0300

    add tasks 1-module 3, 4, 5

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,4 +4,11 @@
  * @returns {string}
  */
 function ucFirst(str) {
+  if (!str) {
+    return str;
+  }
+  if (str.length == 1) {
+    return str.toUpperCase();
+  }
+  return str[0].toUpperCase() + str.slice(1);
 }
