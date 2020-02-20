f651decc862770ec4a3d05e3e7e72fa15d379851
junfrenkixp <penin13579@gmail.com>
2020-02-21 01:57:21 +0300

    add 6.1 task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -17,8 +17,8 @@ function SortableTable(items) {
   /**
    * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
-  this.el = document.createElement('table');
-  this.el.classList.add('table');
+  this.el = document.createElement("table");
+  this.el.classList.add("table");
 
   /**
    * Метод выполняет сортировку таблицы
@@ -27,30 +27,28 @@ function SortableTable(items) {
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
 
-
-  let thead = document.createElement('thead');
-  let trhead = document.createElement('tr');
-  trhead.classList.add('table__row_head');
-  let headInfo = ['Name', 'Age', 'Salary', 'City']; 
+  let thead = document.createElement("thead");
+  let trhead = document.createElement("tr");
+  trhead.classList.add("table__row_head");
+  let headInfo = ["Name", "Age", "Salary", "City"];
 
   headInfo.forEach(item => {
-    let tdhead = document.createElement('td');
-    tdhead.classList.add('table__item');
+    let tdhead = document.createElement("td");
+    tdhead.classList.add("table__item");
     tdhead.innerHTML = item;
     trhead.appendChild(tdhead);
   });
   thead.appendChild(trhead);
   this.el.appendChild(thead);
 
-
-  let tbody = document.createElement('tbody');
+  let tbody = document.createElement("tbody");
 
   items.forEach(item => {
-    let trbody = document.createElement('tr');
-    trbody.classList.add('table__row');
+    let trbody = document.createElement("tr");
+    trbody.classList.add("table__row");
     for (let key in item) {
-      let tdbody = document.createElement('td');
-      tdbody.classList.add('table__item');
+      let tdbody = document.createElement("td");
+      tdbody.classList.add("table__item");
       tdbody.innerHTML = item[key];
       trbody.appendChild(tdbody);
       tbody.appendChild(trbody);
@@ -58,13 +56,14 @@ function SortableTable(items) {
   });
   this.el.appendChild(tbody);
 
-
   this.sort = (column, desc = false) => {
     let trs = Array.from(tbody.rows);
 
     if (column === 0) {
       trs.sort((rowA, rowB) => {
-        return rowA.cells[column].innerHTML > rowB.cells[column].innerHTML ? 1 : -1;
+        return rowA.cells[column].innerHTML > rowB.cells[column].innerHTML
+          ? 1
+          : -1;
       });
       if (desc === true) {
         trs.reverse();
@@ -72,7 +71,7 @@ function SortableTable(items) {
       tbody.append(...trs);
     }
 
-    if (column === 2) {      
+    if (column === 2) {
       trs.sort((rowA, rowB) => {
         return rowA.cells[column].innerHTML - rowB.cells[column].innerHTML;
       });
--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -5,25 +5,88 @@
  * Пример одного элемента, описывающего строку таблицы
  *
  *      {
-     *          name: 'Ilia',
-     *          age: 25,
-     *          salary: '1000',
-     *          city: 'Petrozavodsk'
-     *      },
+ *          name: 'Ilia',
+ *          age: 25,
+ *          salary: '1000',
+ *          city: 'Petrozavodsk'
+ *      },
  *
  * @constructor
  */
 class ClearedTable {
   constructor(data) {
-    this.el = document.createElement('table');
+    this.el = document.createElement("table");
+    this.el.classList.add('pure-table');
     this.data = data;
+    this.tHead = document.createElement("thead");
+    this.trHead = document.createElement("tr");
+    this.tBody = document.createElement("tbody");
+    this.render();
+  }
+
+  thead() {
+    this.tHead.appendChild(this.trHead);
+    for (let key in this.data[0]) {
+      if (key !== 'id') {
+        let td = document.createElement('td');
+        td.innerHTML = key[0].toUpperCase() + key.slice(1);
+        this.trHead.appendChild(td);
+      }
+    }
+    let td = document.createElement('td');
+    this.trHead.appendChild(td);
+    this.el.appendChild(this.tHead);
+  }
+
+  tbody() {
+    this.data.forEach(item => {
+      let trBody = document.createElement("tr");
+      let currentId = "";
+      for (let key in item) {
+        if (key === 'id') {
+          currentId = item[key];
+          continue;
+        }
+        let tdBody = document.createElement("td");
+        tdBody.innerHTML = item[key];
+        trBody.appendChild(tdBody);  
+      }
+      let tdBody = document.createElement("td");
+      let button = document.createElement('a');
+      button.innerHTML = 'X';
+      button.href = '#delete';
+      button.setAttribute('data-delete-id', currentId);
+      tdBody.appendChild(button);
+      trBody.appendChild(tdBody);
+      this.tBody.appendChild(trBody);
+
+    });
+    this.el.appendChild(this.tBody);
+    this.el.addEventListener('click', event => this.deletion(event));
+  }
+
+  render() {
+    this.thead();
+    this.tbody();
+  }
+
+  deletion(event) {
+    let target = event.target;
+    if (target.hasAttribute('data-delete-id')) {
+      let deletedId = +target.getAttribute('data-delete-id');
+      let deletedItem = target.closest('tr');
+      deletedItem.remove();
+      this.onRemoved(deletedId);
+    }
   }
 
   /**
    * Метод который выщывается после удалении строки
    * @param {number} id - идентификатор удаляемого пользователя
    */
-  onRemoved(id) {}
+  onRemoved(id) {
+    console.log(`Deleted item with id ${id}`);
+  }
 }
 
 window.ClearedTable = ClearedTable;
