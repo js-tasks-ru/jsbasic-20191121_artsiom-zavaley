a331ee14bd956d03f984eb24e9e3248acaa9e71c
junfrenkixp <penin13579@gmail.com>
2020-02-22 03:00:30 +0300

    add 4.2 task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,11 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  for (let i = 0; i < table.rows.length; i++) {
+    for (let j = i; j <= table.rows[i].cells.length; j++) {
+      if (i == j) {
+        table.rows[i].cells[j].style.backgroundColor = 'red';
+      }
+    }
+  }
 }
