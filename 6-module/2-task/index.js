c56c6e2498c24f0f059bd2597dcc93a085f62387
junfrenkixp <penin13579@gmail.com>
2020-02-24 04:10:34 +0300

    add 6.2 new modules (test)

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -20,7 +20,14 @@ class Carousel {
   ];
   constructor(element) {
     this.el = element;
+    this.initialSlideId = 0;
+    this.createBasis();
+    this.renderSlide(this.initialSlideId);
+    this.nextSlide();
+    this.prevSlide();
+  }
 
+  createBasis() {
     this.main = document.createElement('div');
     this.main.setAttribute('id', 'mainCarousel');
     this.main.classList.add('main-carousel', 'carousel', 'slide');
@@ -40,48 +47,47 @@ class Carousel {
     this.mainInner = document.createElement('div');
     this.mainInner.classList.add('carousel-inner');
     this.main.appendChild(this.mainInner);
-    this.createSlie();
 
-    let buttonPrev = document.createElement('button');
-    buttonPrev.classList.add('carousel-control-prev');
-    buttonPrev.setAttribute('aria-hidden', true);
+    this.buttonPrev = document.createElement('button');
+    this.buttonPrev.classList.add('carousel-control-prev');
+    this.buttonPrev.setAttribute('aria-hidden', true);
     let buttonPrevIcon = document.createElement('span');
     buttonPrevIcon.classList.add('carousel-control-prev-icon');
     buttonPrevIcon.setAttribute('aria-hidden', true);
-    buttonPrev.appendChild(buttonPrevIcon);
+    this.buttonPrev.appendChild(buttonPrevIcon);
     let buttonPrevContent = document.createElement('span');
     buttonPrevContent.classList.add('sr-only');
     buttonPrevContent.innerHTML = 'Previous';
-    buttonPrev.appendChild(buttonPrevContent);
-    this.main.appendChild(buttonPrev);
+    this.buttonPrev.appendChild(buttonPrevContent);
+    this.main.appendChild(this.buttonPrev);
 
-    let buttonNext = document.createElement('button');
-    buttonNext.classList.add('carousel-control-next');
-    buttonNext.setAttribute('aria-hidden', true);
+    this.buttonNext = document.createElement('button');
+    this.buttonNext.classList.add('carousel-control-next');
+    this.buttonNext.setAttribute('aria-hidden', true);
     let buttonNextIcon = document.createElement('span');
     buttonNextIcon.classList.add('carousel-control-next-icon');
     buttonNextIcon.setAttribute('aria-hidden', true);
-    buttonNext.appendChild(buttonNextIcon);
+    this.buttonNext.appendChild(buttonNextIcon);
     let buttonNextContent = document.createElement('span');
     buttonNextContent.classList.add('sr-only');
     buttonNextContent.innerHTML = 'Next';
-    buttonNext.appendChild(buttonNextContent);
-    this.main.appendChild(buttonNext);
+    this.buttonNext.appendChild(buttonNextContent);
+    this.main.appendChild(this.buttonNext);
   }
 
-  createSlie() {
-    let item = document.createElement('div');
-    item.classList.add('carousel-item', 'active');
-    this.mainInner.appendChild(item);
+  createSlie(imgSrc, title) {
+    this.item = document.createElement('div');
+    this.item.classList.add('carousel-item', 'active');
+    this.mainInner.appendChild(this.item);
 
     let img = document.createElement('img');
-    img.setAttribute('src', 'assets/images/default-slide-img.jpg');
+    img.setAttribute('src', imgSrc);
     img.setAttribute('alt', 'Activelide');
-    item.appendChild(img);
+    this.item.appendChild(img);
 
     let container = document.createElement('div');
     container.classList.add('container');
-    item.appendChild(container);
+    this.item.appendChild(container);
 
     let caption = document.createElement('div');
     caption.classList.add('carousel-caption');
@@ -89,7 +95,7 @@ class Carousel {
 
     let heading = document.createElement('h3');
     heading.classList.add('h1');
-    heading.innerHTML = 'BEST LAPTOP DEALS';
+    heading.innerHTML = title;
     caption.appendChild(heading);
     
     let div = document.createElement('div');
@@ -107,6 +113,40 @@ class Carousel {
     linkImg.setAttribute('alt', '');
     linkImg.classList.add('ml-3');
     link.appendChild(linkImg);
+  }
+
+  renderSlide(id) {
+    console.log(id);
+    switch (id) {
+    case 0: {
+      this.createSlie(this.slides[0].img, this.slides[0].title);
+      break;
+    }
+    case 1: {
+      this.createSlie(this.slides[1].img, this.slides[1].title);
+      break;
+    }
+    case 2: {
+      this.createSlie(this.slides[2].img, this.slides[2].title);
+      break;
+    }
+    }
+  }
+
+  nextSlide() {
+    this.buttonNext.addEventListener('click', () => {
+      this.initialSlideId++;
+      this.item.remove();
+      this.renderSlide(this.initialSlideId);
+    });
+  }
+
+  prevSlide() {
+    this.buttonPrev.addEventListener('click', () => {
+      this.initialSlideId--;
+      this.item.remove();
+      this.renderSlide(this.initialSlideId);
+    });
   }
 }
 
