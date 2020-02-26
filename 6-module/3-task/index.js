3b61221a0b5ca4a2b8cba5b04a29a439704cc7b7
junfrenkixp <penin13579@gmail.com>
2020-02-26 04:17:49 +0300

    add 6.3 task

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -7,7 +7,7 @@ class Menu {
       <a class="nav-link dropdown-toggle" id="cameraphotos">Camera &amp; Photo</a>
       <ul class="dropdown-menu">   
         
-       <li data-id="cameraphotos_accessories" class="dropdown-item"><a>Accessories</a></li>
+        <li data-id="cameraphotos_accessories" class="dropdown-item"><a>Accessories</a></li>
     
       </ul>
     </li>
@@ -16,9 +16,9 @@ class Menu {
       <a class="nav-link dropdown-toggle" id="cinema">Home Cinema, TV &amp; Video</a>
       <ul class="dropdown-menu">   
         
-       <li data-id="cinema_audio" class="dropdown-item"><a>Audio</a></li>
+        <li data-id="cinema_audio" class="dropdown-item"><a>Audio</a></li>
     
-       <li data-id="cinema_video" class="dropdown-item"><a>Video</a></li>
+        <li data-id="cinema_video" class="dropdown-item"><a>Video</a></li>
     
       </ul>
     </li>
@@ -26,6 +26,38 @@ class Menu {
   `;
 
   constructor(element) {
+    this.el = element;
+    this.el.innerHTML = this.template;
+    this.dropdownHover();
+    this.dropdownOnHover();
+  }
+
+  dropdownHover() {
+    const mainItem = this.el.querySelectorAll('.dropdown');
+    const backdrop = document.querySelector('.backdrop');
+    mainItem.forEach(elem => {
+      elem.addEventListener('pointerenter', event => {
+        let target = event.target;
+        let currentItem = target.closest('.dropdown');
+        let subItem = currentItem.querySelector('.dropdown-menu');
+        subItem.classList.add('show');
+        backdrop.classList.add('show');
+      });
+    });
+  }
+
+  dropdownOnHover() {
+    const mainItem = this.el.querySelectorAll('.dropdown');
+    const backdrop = document.querySelector('.backdrop');
+    mainItem.forEach(elem => {
+      elem.addEventListener('pointerleave', event => {
+        let target = event.target;
+        let currentItem = target.closest('.dropdown');
+        let subItem = currentItem.querySelector('.dropdown-menu');
+        subItem.classList.remove('show');
+        backdrop.classList.remove('show');
+      });
+    });
   }
 }
 
